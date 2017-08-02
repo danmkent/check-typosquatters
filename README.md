97a8c98a3dda79491c6e5d0f517b846c91a9738b
dankent <dan.kent@mhub.tv>
2017-08-02 14:51:03 +0100

    Made the tool work properly and added rank to output

--- README.md
+++ README.md
@@ -10,25 +10,25 @@ npm install -g check-typosquatters
 
 Usage
 --------------
-Show 10 npm packages most similarly named to the given parameter
+Show 10 npm packages most similarly named to the given parameter, with their rank (based on number of dependents)
 
 ```sh
-$ check-typosquatters 
+$ check-typosquatters loadash
 
-10 npm packages named most similarly to lodash:
+10 npm packages named most similarly to cross-env:
 
-Edits    Package Name
-----------------------------
-   0      lodash
-   1      loadash
-   2      docdash
-   3      slash
-   4      yosay
-   5      mocha
-   5      tape
-   5      chai
-   6      request
-   6      should
+Edits      Rank   Package Name     
+------------------------------------
+   0       246      cross-env
+   1    520757      crossenv
+   2      7986      crossvent
+   3     19794      cloud-env
+   3     25449      cross-zip
+   3     26607      process-env
+   3     48006      crossref
+   3     74917      chrome-env
+   3     75367      cross-run
+   3     84218      conf-env
 
 It is recommended to update the all-the-package-names package to make sure you have the latest list of packages
 ```
--- bin/check-typosquatters.js
+++ bin/check-typosquatters.js
@@ -12,28 +12,31 @@ if(inputPackageName == null)
 
 console.log("10 npm packages named most similarly to " + inputPackageName + ":");
 
-var smallestDiffSeen = 0;
+var biggestMatchedDiff = 0;
 var bestMatches = [];
 for(let i=0; i< allThePackageNames.length; i++)
 {
-    let diff = levenshtein(allThePackageNames[i], inputPackageName);
+    let diff = Math.abs(levenshtein(allThePackageNames[i], inputPackageName));
 
-    if(bestMatches.length < 10 || diff < smallestDiffSeen)
+    if(bestMatches.length < 10 || diff < biggestMatchedDiff)
     {
-        bestMatches.push({"name": allThePackageNames[i], "diff" : diff});
-        smallestDiffSeen = diff;
+        bestMatches.push({"name": allThePackageNames[i], "diff" : diff, "rank": i});    
+        bestMatches.sort((a,b) => a.diff - b.diff);
+        bestMatches = bestMatches.slice(0,10);
+
+        biggestMatchedDiff =bestMatches[bestMatches.length - 1].diff;
     }
 }
 
 bestMatches.sort((a,b) => a.diff - b.diff);
 
 console.log();
-console.log("Edits    Package Name");
-console.log("----------------------------");
+console.log("Edits      Rank   Package Name     ");
+console.log("------------------------------------");
 
 for(let i=0; i<10; i++)
 {
-    console.log(('    ' + bestMatches[i].diff).slice(-4) + "      " + bestMatches[i].name) ;
+    console.log(('    ' + bestMatches[i].diff).slice(-4) + "  " + ("       " + bestMatches[i].rank).slice(-8) +    "      " + bestMatches[i].name) ;
 }
 console.log();
 console.log("It is recommended to update the all-the-package-names package to make sure you have the latest list of packages");
